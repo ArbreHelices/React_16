1b2fd056c4e7555fdd3eccb4a813745d6f316230
Antoine07 <antoine.lucsko@gmail.com>
2019-11-17 19:01:15 +0100

    correction btn

---
+++ part_hook/btn-count-correction/src/Components/Counter.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect, useRef, useCallback } from 'react';
+
+import Button from './Button';
+
+const Counter = ({ delay }) => {
+
+    const intervalRef = useRef(null);
+
+    const [active, setActive] = useState(false);
+    const [count, setCount] = useState(0);
+
+    // TODO
+
+    return (
+        <>
+          {/* TODO */}
+        </>
+    );
+}
+
+export default Counter;
